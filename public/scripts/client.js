380ac32ec5c404891bab0b51b17c1d5855254a17
Sid Natarajan <sid.nat@gmail.com>
2022-11-10 19:14:06 +0000

    added dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,79 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(function () {
+  const createTweetElement = (tweetData) => {
+    const tweet = `
+      <article class="tweet">
+        <header class="username">
+          <div class="pic-username">
+            <img class="profile-pic" src=${tweetData.user.avatars}>
+            <p>${tweetData.user.name}</p>
+          </div>
+          <p class="userhandle">${tweetData.user.handle}</p>
+        </header class="username">
+        <div class="posted-tweet">
+          <p>${tweetData.content.text}</p>
+        </div>
+        <footer class="metadata">
+          <p>${tweetData.created_at
+          }</p>
+          <div class="retweet-icons">
+            <i class="fa-solid fa-flag icon"></i>
+            <i class="fa-solid fa-retweet icon"></i>
+            <i class="fa-solid fa-heart icon"></i>
+          </div>
+        </footer>
+      </article>
+    `
+
+    return tweet
+  }
+
+  const renderTweets = (arrOfTweets) => {
+    for (let tweet of arrOfTweets) {
+      $('.tweets-container').append(createTweetElement(tweet));
+    }
+  } 
+
+  renderTweets(data);
+});
+
+
+
+// $(function() {
+//   const $button = $('#load-more-posts');
+//   $button.on('click', function () {
+//     console.log('Button clicked, performing ajax call...');
+//     $.ajax('more-posts.html', { method: 'GET' })
+//     .then(function (morePostsHtml) {
+//       console.log('Success: ', morePostsHtml);
+//       $button.replaceWith(morePostsHtml);
+//     });
+//   });
+// });
